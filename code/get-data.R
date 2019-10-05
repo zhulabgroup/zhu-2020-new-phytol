368474f48496bc1f189268e28030a1aaeb685f8d
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2019-10-04 22:22:23 -0700

    Add get data code

---
+++ code/get-data.R
@@ -0,0 +1,13 @@
+# get (download and unzip) raw data
+dir.create('data/', FALSE, TRUE)
+
+# GFAD
+dir.create('data/GFAD', FALSE, TRUE)
+download.file('http://dare.iiasa.ac.at/29/1/GFAD_V1-0.zip', 'data/GFAD/GFAD_V1-0.zip')
+download.file('http://hs.pangaea.de/model/Poulter-etal_2018/GFAD_V1-1.zip', 'data/GFAD/GFAD_V1-1.zip')
+unzip('data/GFAD/GFAD_V1-1.zip', exdir = 'data/GFAD')
+
+# GIS (boundary files)
+dir.create('data/GIS', F, T)
+
+# ForC data
