93b8e4fe380737e5b27a3842d56d3a3b3b0e4291
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2019-10-04 22:29:25 -0700

    Unzip GFAD v1.0

--- code/get-data.R
+++ code/get-data.R
@@ -4,6 +4,7 @@ dir.create('data/', FALSE, TRUE)
 # GFAD
 dir.create('data/GFAD', FALSE, TRUE)
 download.file('http://dare.iiasa.ac.at/29/1/GFAD_V1-0.zip', 'data/GFAD/GFAD_V1-0.zip')
+unzip('data/GFAD/GFAD_V1-0.zip', exdir = 'data/GFAD')
 download.file('http://hs.pangaea.de/model/Poulter-etal_2018/GFAD_V1-1.zip', 'data/GFAD/GFAD_V1-1.zip')
 unzip('data/GFAD/GFAD_V1-1.zip', exdir = 'data/GFAD')
 
